f226d027847eaf17ed5877aaab40fce8ca899004
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-05-28 23:00:31 -0400

    Re-enable repeating tests to verify save/restore
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- test/index.js
+++ test/index.js
@@ -7,6 +7,10 @@ const NETLIFY_CONFIG = fileURLToPath(
 	new URL('../netlify.toml', import.meta.url),
 );
 
+const doNetlifyBuild = async (options) => {
+	return netlifyBuild(options)
+};
+
 // Unit tests are using the AVA test runner: https://github.com/avajs/ava
 // A local build is performed using the following command:
 //   netlify-build --config ../netlify.toml
@@ -14,19 +18,27 @@ const NETLIFY_CONFIG = fileURLToPath(
 // Build plugin.
 // This is a smoke test. You will probably want to write more elaborate unit
 // tests to cover your plugin's logic.
-test('Netlify Build should not fail', async (t) => {
-	const { success, logs } = await netlifyBuild({
-		config: NETLIFY_CONFIG,
-		buffer: true,
-	});
+test('Netlify Build should succeed repeatedly', async (t) => {
+	const result = await doNetlifyBuild(
+		{
+			config: NETLIFY_CONFIG,
+			buffer: false
+		}
+	)
 
-	// Netlify Build output
-	console.log(
-		[logs.stdout.join('\n'), logs.stderr.join('\n')]
-			.filter(Boolean)
-			.join('\n\n'),
-	);
+	var success = await result.success;
 
-	// Check that build succeeded
-	t.true(success);
+	if (success) {
+		const result = await doNetlifyBuild(
+			{
+				config: NETLIFY_CONFIG,
+				buffer: false
+			}
+		)
+		// Check that build succeeded
+		t.true(await result.success);
+	} else {
+		t.true(false) // Force failure of test
+	}
+	t.true(success)
 });
